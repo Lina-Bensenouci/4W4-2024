ef035509927e00570bc3258be706925ee4e94e3a
Lina-Bensenouci <119866419+Lina-Bensenouci@users.noreply.github.com>
2024-03-07 11:23:16 -0500

    adapter le menu en version mobile et changement des vagues et du menu en affichage php

--- index.php
+++ index.php
@@ -1,22 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Theme GR1</title> 
-    <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/normalize.css">
-    <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/style.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Foldit:wght@200;300&family=Playfair+Display:wght@400;500;600;700&family=Prompt:wght@400;500;600;700&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Protest+Revolution&family=Protest+Riot&display=swap" rel="stylesheet">
-  </head>
-  <body>
-    <nav id="menu" class="global">
-      <header class="menu__header">
-          <?php wp_nav_menu(array("container" => "nav")); ?>
-      </header>
-    </nav>
+
+<?php get_header();?>
     <div id="entete" class="global">
       <section class="hero">
         <h1>Thème du groupe #1 (h1)</h1>
@@ -24,11 +7,8 @@
         <h3>TIM - Collège de Maisonneuve</h3>
         <button>Événements</button>
       </section>
-      <div class = "vague">
-        <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-          <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" style = "fill: var(--couleur-arriere-accueil)"></path>
-      </svg>
-      </div>
+      <?php get_template_part('gabarits/vague');?>
+      <!--vague-->
     </div>
     <div id="accueil" class="global">
       <section>
@@ -88,11 +68,7 @@
         <button>Page</button>
       </div>
       </footer>
-      <div class = "vague">
-        <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-          <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" style = "fill: var(--couleur-arriere-accueil)"></path>
-      </svg>
-      </div>
+      <?php get_template_part('gabarits/vague');?>
     </div>
   </body>
 </html>
