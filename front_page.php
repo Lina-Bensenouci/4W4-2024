f02262a4128887d9d0b0f1677b2205a657e127d8
Lina-Bensenouci <119866419+Lina-Bensenouci@users.noreply.github.com>
2024-03-13 20:04:00 -0400

    s6c1 changement de site pour le tp1, nouveaux articles et catégories

--- front_page.php
+++ front_page.php
@@ -1,5 +1,6 @@
 
 <?php get_header();?>
+  <h1>FRONT-PAGE.PHP</h1>
     <div id="entete" class="global">
       <section class="hero">
         <h1>Thème du groupe #1 (h1)</h1>
@@ -31,7 +32,7 @@
           ?>
           <div class="carte">
             <h3><?php echo $titre; ?></h3>
-            <p><?php the_content(); ?> </p>
+            <p><?php echo wp_trim_words(get_the_content(),10); ?></p>
         </div>
         <?php endwhile; ?>
         <?php endif; ?>
