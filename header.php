7806038c5c038906b25d12e6f268801c7f2ecd9f
Lina-Bensenouci <119866419+Lina-Bensenouci@users.noreply.github.com>
2024-03-21 09:20:11 -0400

    Ajustement de la taille du logo

--- header.php
+++ header.php
@@ -13,7 +13,7 @@
   </head>
   <body>
 <div id="menu__entete" class="global">
-  <a href="<?php echo get_bloginfo("url")?>"><img src="<?php echo get_template_directory_uri();?>/images/logoVoyage.png" alt=""></a>
+  <a href="<?php echo get_bloginfo("url")?>"><img src="<?php echo get_template_directory_uri();?>/images/logoVoyage.png" alt="" class="logo"></a>
   <input type="checkbox" name="" id="chk_burger">
     <label for="chk_burger" id="burger">
     <svg  width="32" height="32" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" color="#fff"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
